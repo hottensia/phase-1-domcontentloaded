d907a792ea9c4300797ba84d90600c1e873868d9
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-04-13 18:44:43 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("After DOM loaded");
+    console.log(document.querySelector('p'))
+    const paragraph = document.querySelector('p');
+  paragraph.textContent = "This is really cool!";
+  });
+  
+  console.log("Before DOM loads");
+  console.log(document.querySelector('p'))
